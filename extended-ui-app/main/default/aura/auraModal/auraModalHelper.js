59c7e6c341afc89566cb0a63b2316c5657db5919
Shillem Volpato <volpato.sh@gmail.com>
2020-09-15 09:33:07 +0200

    Created extended-ui-app package

---
+++ extended-ui-app/main/default/aura/auraModal/auraModalHelper.js
@@ -0,0 +1,43 @@
+({
+    calculateStepRange: function (cmp, value) {
+        const steps = cmp.get("v.steps");
+        const index = this.getStepIndex(steps, value);
+
+        cmp.set("v.firstStep", index === 0);
+        cmp.set("v.lastStep", index === steps.length - 1);
+    },
+    fireAction: function (cmp, value) {
+        cmp.getEvent("onaction").setParams({ value }).fire();
+    },
+    getStepIndex: function (steps, stepValue) {
+        return steps.findIndex((step) => step.value === stepValue);
+    },
+    getStepName: function (cmp, mover) {
+        const steps = cmp.get("v.steps");
+        const currentIndex = this.getStepIndex(steps, cmp.get("v.step"));
+
+        return steps[currentIndex + mover].value;
+    },
+    setOpenedClasses: function (cmp) {
+        const opened = cmp.get("v.opened");
+
+        let sectionClass = "slds-modal";
+        let backdropClass = "slds-backdrop";
+
+        if (opened) {
+            const size = cmp.get("v.size");
+
+            if (size) {
+                sectionClass += " slds-modal_" + size;
+            }
+
+            sectionClass += " slds-fade-in-open";
+            backdropClass += " slds-backdrop_open";
+        } else {
+            sectionClass += " slds-hide";
+        }
+
+        cmp.set("v.sectionClass", sectionClass);
+        cmp.set("v.backdropClass", backdropClass);
+    }
+});
---
+++ extended-ui-app/main/default/lwc/modal/modal.js
@@ -0,0 +1,97 @@
+import { api, LightningElement } from "lwc";
+
+export default class Modal extends LightningElement {
+    @api disabled = false;
+    @api i18n = { cancel: "Cancel", close: "Close", next: "Next", ok: "OK", previous: "Previous" };
+    @api loading = false;
+    @api opened = false;
+    @api size;
+    @api step;
+    @api steps;
+    @api tagline;
+    @api title;
+
+    hasActions = false;
+
+    get backdropClass() {
+        let value = "slds-backdrop";
+
+        if (this.opened) {
+            value += " slds-backdrop_open";
+        }
+
+        return value;
+    }
+
+    get closed() {
+        return !this.opened;
+    }
+
+    fireAction(detail) {
+        this.dispatchEvent(new CustomEvent("action", { detail }));
+    }
+
+    handleActionsSlotChange(event) {
+        this.hasActions = event.target.assignedElements().length > 0;
+    }
+
+    handleClose() {
+        this.fireAction({ name: "close" });
+    }
+
+    handleOk() {
+        this.fireAction({ name: "ok" });
+    }
+
+    handleStepNext() {
+        this.fireAction({
+            name: "stepNext",
+            step: this.steps[this.stepIndex + 1]
+        });
+    }
+    
+    handleStepPrevious() {
+        this.fireAction({
+            name: "stepPrevious",
+            step: this.steps[this.stepIndex - 1]
+        });
+    }
+
+    get hasSteps() {
+        return Array.isArray(this.steps) && this.steps.length > 0;
+    }
+
+    get headerClass() {
+        return this.title || this.tagline
+            ? "slds-modal__header"
+            : "slds-modal__header slds-modal__header_empty";
+    }
+
+    get lastStep() {
+        return !this.hasSteps || this.steps[this.steps.length - 1].value === this.step;
+    }
+
+    get sectionClass() {
+        let value = "slds-modal";
+
+        if (this.opened) {
+            if (this.size) {
+                value += " slds-modal_" + this.size;
+            }
+
+            value += " slds-fade-in-open";
+        } else {
+            value += " slds-hide";
+        }
+
+        return value;
+    }
+
+    get stepIndex() {
+        return this.steps.findIndex((step) => step.value === this.step);
+    }
+
+    get subsequentStep() {
+        return this.hasSteps && !this.steps[0].value === this.step;
+    }
+}
