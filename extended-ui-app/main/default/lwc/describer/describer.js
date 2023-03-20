908e241d61b24ce34ab30525b55f9e15a85a4c40
Shillem Volpato <volpato.sh@gmail.com>
2023-03-20 11:41:25 +0200

    Added getSObjectDescriptor lwc method

--- extended-ui-app/main/default/lwc/describer/describer.js
+++ extended-ui-app/main/default/lwc/describer/describer.js
@@ -1,7 +1,14 @@
+import getSObjectDescriptor from "@salesforce/apex/DescriberController.getSObjectDescriptor";
 import getSObjectFields from "@salesforce/apex/DescriberController.getSObjectFields";
 import getSObjectTypes from "@salesforce/apex/DescriberController.getSObjectTypes";
 
 class Describer {
+  asyncGetSObjectDescriptor(query) {
+    return getSObjectDescriptor({
+      query: query ? JSON.stringify(query) : undefined
+    }).then((results) => JSON.parse(results));
+  }
+
   asyncGetSObjectFields(query) {
     return getSObjectFields({
       query: query ? JSON.stringify(query) : undefined
