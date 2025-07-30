f0272824e22437d76b79a73792e09b14b7b234ea
Shillem Volpato <volpato.sh@gmail.com>
2025-07-30 09:21:38 +0300

    Updated linters and code analyzer

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
-module.exports = {
+export default {
   "**/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml}": ["prettier --write"],
-  "**/aura|lwc/**": ["eslint"],
-  "**/*.{cls,trigger}": [
-    "sf code-analyzer run --rule-selector pmd:Apex --rule-selector AppExchange --severity-threshold Low --target"
+  "**/aura|**/lwc|**/*.{cls,trigger}": (filenames) => [
+    `npm run code:analyze -- ${filenames.map((f) => `--target "${f}"`).join(" ")}`
   ]
 };
--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,55 @@
-// eslint.config.js
-const { defineConfig } = require("eslint/config");
-const prettierFlat = require("eslint-config-prettier/flat");
-const prettierRecommended = require("eslint-plugin-prettier/recommended");
-const sfLwcRecommended = require("@salesforce/eslint-config-lwc/recommended");
+import aura from "@salesforce/eslint-plugin-aura";
+import { defineConfig } from "eslint/config";
+import lwcConfig from "@salesforce/eslint-config-lwc";
+import lwcPlugin from "@lwc/eslint-plugin-lwc";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
-module.exports = defineConfig([
+export default defineConfig([
+  prettierRecommended,
   {
-    files: ["force-app/main/default/lwc/**/*.js"],
-    extends: [sfLwcRecommended, prettierRecommended, prettierFlat]
+    files: ["*.js"],
+
+    languageOptions: {
+      ecmaVersion: 6
+    },
+
+    rules: {
+      indent: ["off", "tab"],
+      "linebreak-style": ["error", "unix"],
+      "no-console": "warn",
+      "no-undef": "off",
+
+      "no-unused-vars": [
+        "warn",
+        {
+          args: "none"
+        }
+      ],
+
+      quotes: ["warn", "double"],
+      semi: ["error", "always"]
+    }
+  },
+  {
+    files: ["**/aura/**/*.js"],
+
+    extends: [aura.configs.recommended],
+
+    rules: {
+      "consistent-return": "off",
+      "no-console": "warn",
+      "no-unused-expressions": "off",
+      "no-unused-vars": "warn",
+      "vars-on-top": "off"
+    }
+  },
+  {
+    files: ["**/lwc/**/*.js"],
+
+    extends: [lwcConfig.configs.recommended],
+
+    plugins: {
+      "@lwc/lwc": lwcPlugin
+    }
   }
 ]);
