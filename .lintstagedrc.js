0c0709d03b3256216c926be7c7ebb55cfd83d6f2
Shillem Volpato <volpato.sh@gmail.com>
2025-08-20 10:00:37 +0300

    Updated code after pmd new rules and migrated from npm to bun

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
   "**/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml}": ["prettier --write"],
   "**/aura|**/lwc|**/*.{cls,trigger}": (filenames) => [
-    `npm run code:analyze -- ${filenames.map((f) => `--target "${f}"`).join(" ")}`
+    `bun run code:analyze -- ${filenames.map((f) => `--target "${f}"`).join(" ")}`
   ]
 };
