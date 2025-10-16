65d69067d0111e1d846ad462072cf38983d0aaf8
Shillem Volpato <volpato.sh@gmail.com>
2025-10-16 16:36:04 +0300

    Replaced husky and lint-staged for lefthook

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "**/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml}": ["prettier --write"],
-  "**/aura|**/lwc|**/*.{cls,trigger}": (filenames) => [
-    `bun run code:analyze -- ${filenames.map((f) => `--target "${f}"`).join(" ")}`
-  ]
-};
